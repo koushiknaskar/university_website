c22c247f83d90f2a16a4158e7c75b20908b0b941
koushiknaskar <59178811+koushiknaskar@users.noreply.github.com>
2022-03-08 18:13:54 +0530

    Add files via upload

---
+++ Final Project/scripts/main.js
@@ -0,0 +1,43 @@
+var activeFilter = 'all';
+
+$('.pp-filter-button').on('click', function(e) {
+  $('.pp-filter-button').removeClass('btn-primary');
+  $('.pp-filter-button').addClass('btn-outline-primary');
+  
+  var button = $(this);
+  button.removeClass('btn-outline-primary');
+  button.addClass('btn-primary');
+  filterItems(button.data("filter"));
+  e.preventDefault();
+})
+
+function filterItems(filter) {
+  if(filter === activeFilter) {
+    return;
+  }
+
+  activeFilter = filter;
+  $('.pp-gallery .card').each(function () {
+    var card = $(this);
+    var groups = card.data("groups");
+    var show = false;
+    if(filter === 'all') {
+      show = true;
+    }
+    else {
+      for(var i = 0; i < groups.length; i ++) {
+        if(groups[i] === filter) {
+          show = true;
+        }
+      }
+    }
+    // hide everything first
+    card.fadeOut(400);
+
+    setTimeout(function() {
+      if(show && !card.is(":visible")) {
+          card.fadeIn(400)
+        }
+      }, 500);
+  });
+}
